5fd8dd4d65ab89300bb25054929b75ae71ae57a0
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2025-08-12 09:22:55 +0530

    Create App.jsx

---
+++ Assignment 16/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileCard from './components/ProfileCard';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 dark:from-gray-800 dark:to-black">
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
